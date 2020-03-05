15c5114f75212ffc0aaa5952f1196564f752486f
Linzertorte <Linzertorte@users.noreply.github.com>
2020-03-04 16:07:37 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import send_task
-# from weibo.weibo_login import wblogin
-from weibo.weibo_qrcode import wblogin
+from weibo.weibo_sender import WeiboSender
+from weibo.weibo_login import wblogin
+from weibo.weibo_message import WeiboMessage
 
 if __name__ == '__main__':
     (wei_session, uid) = wblogin()
     if uid is not None:
         wei_session.get('http://weibo.com/')
-        task = send_task.SendTask(wei_session, uid)
-        task.start()
-
-        while True:
-            cmd = input('enter [exit] to stop:')
-            if cmd.upper() == "EXIT":
-                task.stop()
-                break
-
-    print('exit...')
+        print uid
+        sender = WeiboSender(wei_session, uid)
+        weibo = WeiboMessage("test",["https://github.com/Linzertorte/linzertorte.github.io/blob/master/3.png?raw=true"])
+        sender.send_weibo(weibo)
